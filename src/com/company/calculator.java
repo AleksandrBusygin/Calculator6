bea87e1393b27eeaf2964939f98ab01b314d52a6
Aleksandr Busygin <persik665@yandex.ru>
2019-03-07 00:31:39 +0300

    массив

--- src/com/company/calculator.java
+++ src/com/company/calculator.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static com.company.massiv.maxWord;
+
 /**
  * The program performs simple math operations.
  * @author Aleksandr Busygin
@@ -20,51 +22,72 @@ import java.io.InputStreamReader;
 
 public class calculator {
 
-        public static double plus(double a, double b) {
-            return a+b;
-        }
+    public static double plus(double a, double b) {
+        return a + b;
+    }
 
-        public static double minus(double a, double b) {
-            return a-b;
-        }
+    public static double minus(double a, double b) {
+        return a - b;
+    }
 
-        public static double multiplication(double a, double b) {
-            return a*b;
-        }
+    public static double multiplication(double a, double b) {
+        return a * b;
+    }
 
-        public static double division(double a, double b) {
-            return (double) a/b;
-        }
+    public static double division(double a, double b) {
+        return (double) a / b;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Введите первое число:");
-        double first = Double.parseDouble(reader.readLine());
-        System.out.println("Введите второе число:");
-        double second = Double.parseDouble(reader.readLine());
-        System.out.println("Введите необходимую операцию (или +, или -, или *, или /):");
-        String op = reader.readLine();
+        while (true) {
+            System.out.println("Введите номер задания: 1 - калькулятор, 2 – массив, 0 – выход из программы");
+            String s = reader.readLine();
+            int a = Integer.parseInt(s);
+            if (!(a==0 || a==1 ||a==2)){
+                System.out.println("Неверное значение, введите номер задания: 1 - калькулятор, 2 – массив, 0 – выход из программы ");
+                continue;
+            }
+            if (a == 0) {
+                break;
+            }
+            if (a == 1) {
+                System.out.println("Введите первое число:");
+                double first = Double.parseDouble(reader.readLine());
+                System.out.println("Введите второе число:");
+                double second = Double.parseDouble(reader.readLine());
+                System.out.println("Введите необходимую операцию (или +, или -, или *, или /):");
+                String op = reader.readLine();
 
-        if (op.equals("+"))
-        {
-            System.out.printf("Cумма равна %.4f", plus(first,second));
-        }
-        else if (op.equals("-"))
-        {
-            System.out.printf("Разность равна %.4f", minus(first,second));
-        }
-        else if (op.equals("*"))
-        {
-            System.out.printf("Результат умножения равен %.4f", multiplication(first,second));
-        }
-        else if (op.equals("/"))
-        {
-            System.out.printf("Результат деления равен %.4f", division(first,second));
-        }
-        else
-        {
-            System.out.println("Неверное значение для определения операции");
+                if (op.equals("+")) {
+                    System.out.printf("Cумма равна %.4f", plus(first, second) + '\n');
+                    System.out.println("");
+                } else if (op.equals("-")) {
+                    System.out.printf("Разность равна %.4f", minus(first, second));
+                    System.out.println("");
+                } else if (op.equals("*")) {
+                    System.out.printf("Результат умножения равен %.4f", multiplication(first, second));
+                    System.out.println("");
+                } else if (op.equals("/")) {
+                    System.out.printf("Результат деления равен %.4f", division(first, second));
+                    System.out.println("");
+                } else {
+                    System.out.println("Неверное значение для определения операции");
+                }
+            } else if (a == 2) {
+                System.out.println("Введите количество элементов массива:");
+                int countOfelements = Integer.parseInt(reader.readLine());
+                System.out.println("Заполните массив словами:");
+                String[] array = new String[countOfelements];
+                for (int i = 0; i < array.length; i++) {
+                    array[i] = reader.readLine();
+                }
+                System.out.println("Самое длинное слово: " + maxWord(array));
+            } else {
+                System.out.println("Введен неверный номер задания");
+            }
+
         }
     }
 }
